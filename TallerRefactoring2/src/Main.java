1cca94a268b0deb144592394e5392a339e68d90f
Daniela Astudillo <danvastu@espol.edu.ec>
2026-01-07 14:27:22 -0500

    Refactorización de Paciente
    Se privatizó el historial medico.
    Se creo el getter del historial
    Se creo constante para edad

--- TallerRefactoring2/src/Main.java
+++ TallerRefactoring2/src/Main.java
@@ -11,7 +11,7 @@ public class Main{
 
     public static void inicializar(){
         sistemaAtencionMedica = new SistemaAtencionMedico();
-        Paciente paciente = new Paciente("Dario",21,"Masculino","Floresta 2","0948759754","dlaborde@espol.edu.ec");
+        Paciente paciente = new Paciente("Dario",21,"Masculino","Floresta 2","0948759754","dlaborde@espol.edu.ec", new HistorialMedico());
         Medico medico = new Medico("Nohelia",20,"Femenino","Duran","0991459625","nohePasaca@espol.edu.ec");
         ServicioMedico servicioMedico = new ServicioMedico("Psiquiatria","rama que trata problemas mentales",45.5,40);
         sistemaAtencionMedica.agregarPaciente(paciente);
--- TallerRefactoring2/src/Paciente.java
+++ TallerRefactoring2/src/Paciente.java
@@ -1,11 +1,25 @@
 public class Paciente extends Persona {
-    public HistorialMedico historialMedico;
-    public Paciente(String nombre, int edad, String genero, String direccion, String telefono, String correoElectronico) {
-        super(nombre, edad, genero, direccion, telefono, correoElectronico);
-        this.historialMedico = new HistorialMedico();
+    //Encapsulamiento de historialMedico
+    private HistorialMedico historialMedico;
+
+    //Constante para la edad de adulto mayor
+    public static final int EDAD_ADULTO_MAYOR = 65;
+    
+    //Constructor de la clase Paciente
+    public Paciente(String nombre, int edad, String genero, String direccion, String telefono, String correoElectronico, HistorialMedico historial) {
+            super(nombre, edad, genero, direccion, telefono, correoElectronico);
+            this.historialMedico = historial;
     }
 
+    //Método para determinar si el paciente es adulto mayor
     public boolean esAdultoMayor(){
-        return super.getEdad()>=65;
+        //Cambio en la condición para usar la constante EDAD_ADULTO_MAYOR
+        return super.getEdad()>=EDAD_ADULTO_MAYOR;
+    }
+
+    //Getter para historialMedico
+    public HistorialMedico getHistorialMedico() {
+        return historialMedico;
     }
 }
+
